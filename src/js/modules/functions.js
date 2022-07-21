8b5359633e00eea718541ca439b90c43c252d716
Stark <serj.elessar@gmail.com>
2022-07-21 20:35:06 +0300

    create func

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -32,3 +32,99 @@ export function windowPopup() {
     }
   }
 }
+
+export function changeValue() {
+  const formAction = document.querySelectorAll('form.promo__form')
+
+  const chainsawInfo = {
+    shtenli: {
+      550: {
+        id: '550',
+        price: 508,
+        weight: '6,5',
+      },
+      520: {
+        id: '520',
+        price: 485,
+        weight: '6,3',
+      },
+      450: {
+        id: '450',
+        price: 466,
+        weight: '6,2',
+      },
+      350: {
+        id: '350',
+        price: 422,
+        weight: '6,0',
+      },
+    },
+    profi: {
+      550: {
+        id: '5500',
+        price: 420,
+        weight: '6,5',
+      },
+      520: {
+        id: '5200',
+        price: 398,
+        weight: '6,5',
+      },
+      450: {
+        id: '4500',
+        price: 376,
+        weight: '6,5',
+      },
+      350: {
+        id: '3500',
+        price: 332,
+        weight: '6,3',
+      },
+    },
+  }
+  const currentSet = {
+    brand: 'shtenli',
+    value: 550,
+    setCurrentValue() {
+      return chainsawInfo[this.brand][this.value]
+    }
+  }
+
+  function animationNumber(currentPrice, priceLabel) {
+    const time        = 150
+    const stepNumber  = 72
+    let counterPrice  = 0
+
+    const timeInterval = Math.round(time / (currentPrice / stepNumber))
+    const intervalChangeNumber = setInterval(() => {
+      counterPrice += stepNumber
+
+      if (counterPrice >= currentPrice) {
+        clearInterval(intervalChangeNumber)
+        priceLabel.innerHTML = currentPrice
+      } else {
+        priceLabel.innerHTML = counterPrice
+      }
+    }, timeInterval)
+  }
+
+  formAction.forEach((form) => {
+    function setValue() {
+      const priceLabel = form.querySelector('.price--new')
+      const titleLabel = form.querySelector('.product-id')
+      const weightLabel = form.querySelector('.product-weight')
+
+      currentSet.brand = form.querySelector("input[name='product']").value
+      currentSet.value = form.querySelector('input[name="chainsaw-shtenli"]:checked').value
+
+      priceLabel.innerHTML = currentSet.setCurrentValue().price
+      weightLabel.innerHTML = currentSet.setCurrentValue().weight
+      titleLabel.innerHTML = currentSet.setCurrentValue().id
+
+      animationNumber(currentSet.setCurrentValue().price, priceLabel)
+    }
+
+    setValue()
+    form.addEventListener('input', setValue)
+  })
+}
