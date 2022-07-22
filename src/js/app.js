040eeb83bb3f3148d4660b3a5b72576e2201a180
Stark <serj.elessar@gmail.com>
2022-07-22 22:58:08 +0300

    import new func

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,7 @@ import * as flsFunctions from './modules/functions.js'
 flsFunctions.isWebp()
 flsFunctions.windowPopup()
 flsFunctions.changeValue()
+flsFunctions.stickyHeader()
 
 import Swiper, { Pagination, Autoplay, EffectFade } from 'swiper'
 
